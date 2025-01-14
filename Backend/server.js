6fa128090c2999420fc06a856a08e5c86b1f405b
Ishika Goyal <goyalishika2004@gmail.com>
2025-01-14 11:53:20 +0530

    backend connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,30 @@
 // express server initialization
+
 const express = require('express');
 const app = express();
-const port = 3000;  
+const port = 3000; 
+const cors=require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST'], 
+    credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
 app.listen(port, () => {    
     console.log(`Server running on port ${port}`);
 }); 
-//to display somtthing on home page
-app.get('/home', (req, res) => {
+//to display something on home page
+app.get('/', (req, res) => {
     res.send('Welcome to Express');
 });
+
+app.get('/api/test', (req, res) => {
+    res.json({ success: true, message: 'Frontend and backend are connected!' });
+  });
 
 
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
-
+import axios from 'axios';
+import {useEffect} from 'react';
 function App() {
   
+  useEffect(()=>{
+    
+    axios.get('http://localhost:3000/api/test') 
+    .then((response) => {
+      console.log(response.data);
+      
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+},[]);
 
   return (
     <>
